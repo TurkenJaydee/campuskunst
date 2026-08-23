06ed1d0d690891a1483647943ba70b5dbd02dcd4
TurkenJaydee <alexandergrabsch@gmx.de>
2026-08-23 13:03:47 +0200

    fix scrollbar issues

--- src/components/App.js
+++ src/components/App.js
@@ -7,70 +7,114 @@ import { ThemeProvider } from "@material-ui/styles";
 import SideDrawer from "./SideDrawer/SideDrawer";
 import Toolbar from "./Toolbar/Toolbar";
 import { Helmet } from "react-helmet";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+  appContainer: {
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "100vh",
+  },
+  mainContent: {
+    flexGrow: 1,
+  },
+}));
 
 const App = () => {
   const [sideDrawerOpen, setSideDrawer] = useState(false);
-
   const [fontSize, setFontSize] = useState();
+  const classes = useStyles();
 
   const calcFontSize = (value) => {
     return fontSize ? `${value * `1.${fontSize}`}rem` : `${value}rem`;
   };
 
   const theme = createMuiTheme({
-
-    /* palette: {
-      type: 'dark',
-    }, */
-
+    overrides: {
+      MuiCssBaseline: {
+        "@global": {
+          html: {
+            overflowY: "scroll !important",
+          },
+        },
+      },
+      MuiDrawer: {
+        paper: {
+          "@media (max-width:599px)": {
+            width: "12rem",
+          },
+          "@media (min-width:600px)": {
+            width: "16rem",
+          },
+          "@media (min-width:960px)": {
+            width: "20rem",
+          },
+        },
+      },
+      MuiOutlinedInput: {
+        root: {
+          fontSize: calcFontSize(1.3),
+        },
+      },
+      MuiButton: {
+        containedSizeLarge: {
+          fontSize: calcFontSize(0.9),
+        },
+      },
+      MuiChip: {
+        root: {
+          fontSize: calcFontSize(0.8125),
+        },
+        sizeSmall: {
+          height: calcFontSize(1.5),
+          borderRadius: "100rem",
+        },
+        outlined: {
+          fontSize: calcFontSize(1),
+          height: calcFontSize(2.5),
+        },
+        clickable: {
+          fontSize: calcFontSize(1),
+          height: calcFontSize(2.5),
+        },
+      },
+    },
     typography: {
       fontFamily: ["Muli", "Montserrat", "Helvetica Neue", "sans-serif"].join(","),
-
       body1: {
         fontSize: calcFontSize(1),
         lineHeight: calcFontSize(1.5),
-
         "@media (min-width:420px)": {
           fontSize: calcFontSize(1.3),
           lineHeight: calcFontSize(2),
         },
       },
-
       body2: {
         fontSize: calcFontSize(1),
         lineHeight: calcFontSize(1.5),
-
         "@media (min-width:600px)": {
           fontSize: calcFontSize(1.3),
           lineHeight: calcFontSize(2),
         },
       },
-
       subtitle1: {
         fontSize: calcFontSize(1.3),
       },
-
       subtitle2: {
         fontSize: calcFontSize(1.1),
       },
-
       h1: {
         fontSize: calcFontSize(1.5),
-
         "@media (min-width:600px)": {
           fontSize: calcFontSize(2),
         },
-
         "@media (min-width:960px)": {
           fontSize: calcFontSize(2.5),
         },
-
       },
-
       h2: {
         fontSize: calcFontSize(2.5),
         lineHeight: calcFontSize(3),
-
         "@media (min-width:600px)": {
           fontSize: calcFontSize(3.75),
         },
@@ -78,14 +122,12 @@ const App = () => {
       h4: {
         fontSize: calcFontSize(1.8),
         lineHeight: calcFontSize(3),
-
         "@media (min-width:600px)": {
           fontSize: calcFontSize(2.1),
         },
       },
       h5: {
         fontSize: calcFontSize(1.2),
-
         "@media (min-width:600px)": {
           fontSize: calcFontSize(1.5),
         },
@@ -93,58 +135,6 @@ const App = () => {
       caption: {
         fontSize: calcFontSize(1.5),
       },
-    },
-
-    overrides: {
-
-      MuiDrawer: {
-        paper: {
-
-          "@media (max-width:599px)": {
-            width: '12rem',
-          },
-
-          "@media (min-width:600px)": {
-            width: '16rem',
-          },
-
-          "@media (min-width:960px)": {
-            width: '20rem',
-          },
-        },
-      },
-
-      MuiOutlinedInput: {
-        root: {
-          fontSize: calcFontSize(1.3),
-        },
-      },
-
-      MuiButton: {
-        containedSizeLarge: {
-          fontSize: calcFontSize(0.9),
-        }
-      },
-      MuiChip: {
-        root: {
-          fontSize: calcFontSize(0.8125),
-        },
-
-        sizeSmall: {
-          height: calcFontSize(1.5),
-          borderRadius: '100rem',
-        },
-
-        outlined: {
-          fontSize: calcFontSize(1),
-          height: calcFontSize(2.5),
-        },
-
-        clickable: {
-          fontSize: calcFontSize(1),
-          height: calcFontSize(2.5),
-        },
-      }
     },
   });
 
@@ -157,18 +147,28 @@ const App = () => {
   };
 
   return (
-    <div>
+    <div className={classes.appContainer}>
       <Helmet>
-          <title>campuskunst</title>
-        </Helmet>
+        <title>campuskunst</title>
+      </Helmet>
       <ThemeProvider theme={theme}>
         <Fragment>
           <CssBaseline />
           <Router>
             <Fragment>
-              <Toolbar role="navigation" drawerClickHandler={drawerToggleClickHandler} setFontSize={(e) => setFontSize(e)} />
+              <Toolbar
+                role="navigation"
+                drawerClickHandler={drawerToggleClickHandler}
+                setFontSize={(e) => setFontSize(e)}
+              />
             </Fragment>
-            <SideDrawer show={sideDrawerOpen} close={drawerToggleClickHandler} closeOnClick={drawerCloseHandler}/>
+            <SideDrawer
+              show={sideDrawerOpen}
+              close={drawerToggleClickHandler}
+              closeOnClick={drawerCloseHandler}
+            />
+            <main className={classes.mainContent}>
+            </main>
           </Router>
           <Footer />
         </Fragment>
