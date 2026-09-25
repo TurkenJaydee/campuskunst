43b992cb42aad673a76d30443c3f2050a97ca2e9
TurkenJaydee <44162503+TurkenJaydee@users.noreply.github.com>
2026-09-25 13:18:27 +0200

    make start and slogan accessible

--- src/components/Slogan/Slogan.js
+++ src/components/Slogan/Slogan.js
@@ -2,105 +2,110 @@ import React, { Fragment } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 
-const Slogan = () => {
-  const useStyles = makeStyles((theme) => ({
-    sloganWrapper: {
-      backgroundImage: `url(${process.env.PUBLIC_URL + "/img/congruent_pentagon_blue.png"})`,
-      backgroundPosition: "center center",
-      padding: "7rem 0 7rem 0",
+const useStyles = makeStyles((theme) => ({
+  sloganWrapper: {
+    backgroundImage: `url(${process.env.PUBLIC_URL + "/img/congruent_pentagon_blue.png"})`,
+    backgroundPosition: "center center",
+    padding: "7rem 0 7rem 0",
 
-      [theme.breakpoints.down(900)]: {
-        padding: "4rem 0 7rem 0",
-      },
-
-      [theme.breakpoints.down(600)]: {
-        padding: "3rem 0 7rem 0",
-      },
-
-      [theme.breakpoints.down(420)]: {
-        backgroundSize: "cover",
-      },
-
-      [theme.breakpoints.up(420)]: {
-        backgroundSize: "contain",
-      },
+    [theme.breakpoints.down(900)]: {
+      padding: "4rem 0 7rem 0",
     },
 
-    slogan: {
-      marginTop: "4rem",
-      fontWeight: "100",
-      padding: "2rem 0 0",
-      textTransform: "uppercase",
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
+    [theme.breakpoints.down(600)]: {
+      padding: "3rem 0 7rem 0",
     },
 
-    sloganIntro: {
-      borderBottom: "1px solid #000",
-      paddingBottom: ".25rem",
-      color: "#333f53",
-      fontWeight: "400",
-      lineHeight: "1.767",
-
-      [theme.breakpoints.up("xs")]: {
-        fontSize: "0.875rem",
-      },
-
-      [theme.breakpoints.up("sm")]: {
-        fontSize: "1.125rem",
-      },
+    [theme.breakpoints.down(420)]: {
+      backgroundSize: "cover",
     },
 
-    sloganMain: {
-      borderBottom: "1px solid #000",
+    [theme.breakpoints.up(420)]: {
+      backgroundSize: "contain",
+    },
+  },
 
-      fontWeight: "100",
-      letterSpacing: ".06rem",
-      lineHeight: "1.125",
-      padding: "0 0 .25rem",
-      textTransform: "none",
+  slogan: {
+    marginTop: "4rem",
+    fontWeight: "100",
+    padding: "2rem 0 0",
+    textTransform: "uppercase",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
 
-      [theme.breakpoints.up("xs")]: {
-        fontSize: "1.92rem",
-      },
+  sloganIntro: {
+    borderBottom: "1px solid #000",
+    paddingBottom: ".25rem",
+    color: "#333f53",
+    fontWeight: "400",
+    lineHeight: "1.767",
 
-      [theme.breakpoints.up("sm")]: {
-        fontSize: "2.92rem",
-      },
+    [theme.breakpoints.up("xs")]: {
+      fontSize: "0.875rem",
     },
 
-    sloganSubline: {
-      color: "#333f53",
+    [theme.breakpoints.up("sm")]: {
       fontSize: "1.125rem",
-      fontWeight: "400",
-      lineHeight: "1.767",
+    },
+  },
 
-      [theme.breakpoints.up("xs")]: {
-        fontSize: "0.875rem",
-      },
+  sloganMain: {
+    borderBottom: "1px solid #000",
+    fontWeight: "100",
+    letterSpacing: ".06rem",
+    lineHeight: "1.125",
+    padding: "0 0 .25rem",
+    textTransform: "none",
+    margin: "0.5rem 0", // Leichtes Margin für bessere Abstände
 
-      [theme.breakpoints.up("sm")]: {
-        fontSize: "1.125rem",
-      },
+    [theme.breakpoints.up("xs")]: {
+      fontSize: "1.92rem",
     },
 
-    "& p": {
-      margin: "0",
+    [theme.breakpoints.up("sm")]: {
+      fontSize: "2.92rem",
     },
-  }));
+  },
 
+  sloganSubline: {
+    color: "#333f53",
+    fontSize: "1.125rem",
+    fontWeight: "400",
+    lineHeight: "1.767",
+
+    [theme.breakpoints.up("xs")]: {
+      fontSize: "0.875rem",
+    },
+
+    [theme.breakpoints.up("sm")]: {
+      fontSize: "1.125rem",
+    },
+  },
+}));
+
+const Slogan = () => {
   const classes = useStyles();
 
   return (
     <Fragment>
       <div className={classes.sloganWrapper}>
         <div className={classes.slogan}>
-          <Typography className={classes.sloganIntro}>Ein Projekt des Uni-Archivs Bremen</Typography>
-          <Typography className={classes.sloganMain}>
-            <span>CAMPUSKUNST</span>
+          {/* Semantic Fix: Explizites <p> Tag für den Einleitungstext */}
+          <Typography component="p" className={classes.sloganIntro}>
+            Ein Projekt des Uni-Archivs Bremen
           </Typography>
-          <Typography className={classes.sloganSubline}>Kunstwerke an der Uni Bremen</Typography>
+          
+          {/* Semantic Fix: h2 als logische Überschrift für diesen Seiten-Header */}
+          <Typography component="h2" className={classes.sloganMain}>
+            CAMPUSKUNST
+          </Typography>
+          
+          {/* Semantic Fix: Explizites <p> Tag für den Untertitel */}
+          <Typography component="p" className={classes.sloganSubline}>
+            Kunstwerke an der Uni Bremen
+          </Typography>
         </div>
       </div>
     </Fragment>
--- src/components/Start/Start.js
+++ src/components/Start/Start.js
@@ -12,9 +12,10 @@ import {databases} from '../../localapi/databases.enum';
 import SpinningCircle from "../SpinningCircle/SpinningCircle";
 import { Helmet } from "react-helmet";
 
-const start = () => {
+const Start = () => {
   const [homeContent, setHomeContent] = useState([{}]);
   const [toggleEasyText, setToggleEasyText] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -22,9 +23,15 @@ const start = () => {
   }, []);
 
   const fetchContent = async () => {
-    const data = await fetch(API_PATH(databases.start));
-    const resData = await data.json();
-    setHomeContent(resData);
+    try {
+      const data = await fetch(API_PATH(databases.start));
+      const resData = await data.json();
+      setHomeContent(resData);
+    } catch (error) {
+      console.error("Fehler beim Laden des Startseiteninhalts", error);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   const onToggleEasyText = () => {
@@ -32,7 +39,7 @@ const start = () => {
   };
 
   const getTextStyle = (index) => {
-    if (homeContent[index] !== undefined) {
+    if (homeContent[index] !== undefined && Object.keys(homeContent[index]).length > 0) {
       return toggleEasyText ? homeContent[index].description_easy : homeContent[index].description;
     } else {
       return "Inhalt nicht gefunden.";
@@ -40,23 +47,21 @@ const start = () => {
   };
 
   const getImage = (index) => {
-    if (homeContent[index] !== undefined) {
+    if (homeContent[index] !== undefined && Object.keys(homeContent[index]).length > 0) {
       if(homeContent[index].image !== '') {
         return homeContent[index].image;
       }
-    } else {
-      return "Bild nicht gefunden.";
     }
+    return "platzhalter.jpg";
   };
 
   const getImageAlt = (index) => {
-    if (homeContent[index] !== undefined) {
+    if (homeContent[index] !== undefined && Object.keys(homeContent[index]).length > 0) {
       if(homeContent[index].image_alt !== '') {
         return homeContent[index].image_alt;
       }
-    } else {
-      return "Bild nicht gefunden.";
     }
+    return ""; 
   };
 
   const useStyles = makeStyles((theme) => ({
@@ -99,32 +104,14 @@ const start = () => {
 
     goals: {
       backgroundColor: "rgba(250, 250, 250)",
-
-      "& h1": {
-        [theme.breakpoints.down("sm")]: {
-          textAlign: "center",
-        },
-      },
     },
 
     premises: {
       backgroundColor: "rgba(0, 0, 0, 0.04);",
-
-      "& h1": {
-        [theme.breakpoints.down("sm")]: {
-          textAlign: "center",
-        },
-      },
     },
 
     exhibition: {
       backgroundColor: "rgba(250, 250, 250);",
-
-      "& h1": {
-        [theme.breakpoints.down("sm")]: {
-          textAlign: "center",
-        },
-      },
     },
 
     premiseList: {
@@ -154,6 +141,11 @@ const start = () => {
 
     firstChapter: {
       marginBottom: "1.5rem",
+      textAlign: "left",
+      lineHeight: "1.2 !important", 
+      [theme.breakpoints.down("sm")]: {
+        textAlign: "center",
+      },
     },
 
     formControl: {
@@ -181,124 +173,112 @@ const start = () => {
 
   const classes = useStyles();
 
-  if (homeContent.length > 0) {
-    return (
-      <Fragment>
-        <Helmet>
-          <title>Start</title>
-        </Helmet>
-        <Fade in={true} timeout={1500}>
-          <Typography role="main" component={"span"}>
-            <div className={`${classes.section} ${classes.art}`}>
-              <Container maxWidth="md">
-                <FormControlLabel
-                  className={classes.formControl}
-                  control={
-                    <Switch
-                      checked={toggleEasyText}
-                      onChange={onToggleEasyText}
-                      color="primary"
-                      name="Einfache Sprache Button"
-                      size="medium"
-                    />
-                  }
-                  label="Einfache Sprache"
-                />
-                <h1>
-                  <Typography className={`${classes.firstChapter} ${classes.heading}`} variant="h4" variantMapping={{ h4: "span" }}>
-                    <Typography variant="h2" variantMapping={{ h2: "span" }} display="inline" className={classes.keyword}>
-                      KUNST
-                    </Typography>{" "}
-                    <span className={classes.sectionHeading}>AN DER UNIVERSITÄT BREMEN </span>
-                  </Typography>
-                </h1>
-                <PaletteIconOutlinedIcon aria-hidden="true" />
-                <PaletteIconOutlinedIcon aria-hidden="true" />
-                <PaletteIconOutlinedIcon aria-hidden="true" />
-                <Typography variant="body1" className={`${classes.content} ${classes.firstContent}`} dangerouslySetInnerHTML={{ __html: getTextStyle(0) }}></Typography>
-              </Container>
-            </div>
-            <div className={`${classes.section} ${classes.program}`}>
-              <Container maxWidth="lg">
-                <Grid container spacing={3} alignItems="center">
-                  <Grid item sm={12} md={6}>
-                    <img className={classes.image} alt={getImageAlt(1)} src={`${process.env.PUBLIC_URL + `/img/${getImage(1)}`}`}></img>
-                  </Grid>
-                  <Grid item sm={12} md={6}>
-                    <Typography variant="h4" variantMapping={{ h4: "h2" }} className={classes.heading}>
-                      <Typography variant="h2" variantMapping={{ h2: "span" }} display="inline" className={classes.keyword}>
-                        PROGRAMM
-                      </Typography>{" "}
-                      "KUNST IM UNIVERSITÄTSBEREICH"
-                    </Typography>
-                    <Typography className={classes.content} dangerouslySetInnerHTML={{ __html: getTextStyle(1) }}></Typography>
-                  </Grid>
-                </Grid>
-              </Container>
-            </div>
-            <div className={`${classes.section} ${classes.goals}`}>
-              <Container maxWidth="lg">
-                <Grid container spacing={3} alignItems="center">
-                  <Grid item sm={12} md={6}>
-                    <Typography variant="h4" variantMapping={{ h4: "h2" }} className={classes.heading}>
-                      <Typography variant="h2" variantMapping={{ h2: "span" }} display="inline" className={classes.keyword}>
-                        ZIELE{" "}
-                      </Typography>{" "}
-                      DES BREMER HOCHSCHULMODELLS
-                    </Typography>
-                    <Typography className={classes.content} dangerouslySetInnerHTML={{ __html: getTextStyle(2) }}></Typography>
-                  </Grid>
-                  <Grid item sm={12} md={6}>
-                    <img className={classes.image} alt={getImageAlt(2)} src={`${process.env.PUBLIC_URL + `/img/${getImage(2)}`}`}></img>
-                  </Grid>
-                </Grid>
-              </Container>
-            </div>
-            <div className={`${classes.section} ${classes.premises}`}>
-              <Container maxWidth="lg">
-                <Grid container spacing={3} alignItems="center">
-                  <Grid className={classes.premiseListImg} item sm={12} md={6}>
-                    <img className={classes.image} alt={getImageAlt(3)} src={`${process.env.PUBLIC_URL + `/img/${getImage(3)}`}`}></img>
-                  </Grid>
-                  <Grid className={classes.premiseList} item sm={12} md={6}>
-                    <div>
-                      <Typography variant="h4" variantMapping={{ h4: "h2" }} className={classes.heading}>
-                        ES GALTEN ZWEI{" "}
-                        <Typography variant="h2" variantMapping={{ h2: "span" }} display="inline" className={classes.keyword}>
-                          PRÄMISSEN
-                        </Typography>
-                      </Typography>
-                      <Typography className={classes.content} dangerouslySetInnerHTML={{ __html: getTextStyle(3) }}></Typography>
-                    </div>
-                  </Grid>
-                </Grid>
-              </Container>
-            </div>
-            <div className={`${classes.section} ${classes.exhibition}`}>
-              <Container maxWidth="lg">
-                <Grid container spacing={3} alignItems="center">
-                  <Grid item sm={12} md={6}>
-                  <Typography variant="h4" variantMapping={{ h4: "h2" }} className={classes.heading}>
-                        AUSSTELLUNG{" "}
-                        <Typography variant="h2" variantMapping={{ h2: "span" }} display="inline" className={classes.keyword}>
-                          KUNST IM STADTBILD
-                        </Typography>
-                      </Typography>
-                    <Typography className={classes.content} dangerouslySetInnerHTML={{ __html: getTextStyle(4) }}></Typography>
-                  </Grid>
-                  <Grid item sm={12} md={6}>
-                    <img className={classes.image} alt={getImageAlt(4)} src={`${process.env.PUBLIC_URL + `/img/${getImage(4)}`}`}></img>
-                  </Grid>
-                </Grid>
-              </Container>
-            </div>
-          </Typography>
-        </Fade>
-      </Fragment>
-    );
-  } else {
-    return <SpinningCircle />;
+  if (isLoading) {
+    return <SpinningCircle aria-label="Lade Startseite" />;
   }
+
+  return (
+    <Fragment>
+      <Helmet>
+        <title>Start - Campuskunst</title>
+      </Helmet>
+      <Fade in={true} timeout={1500}>
+        <main>
+          <section className={`${classes.section} ${classes.art}`} aria-labelledby="heading-art">
+            <Container maxWidth="md">
+              <FormControlLabel
+                className={classes.formControl}
+                control={
+                  <Switch
+                    checked={toggleEasyText}
+                    onChange={onToggleEasyText}
+                    color="primary"
+                    name="Einfache Sprache Button"
+                    size="medium"
+                    aria-label="Einfache Sprache ein und ausschalten"
+                    aria-checked={toggleEasyText}
+                  />
+                }
+                label="Einfache Sprache"
+              />
+              
+              <Typography id="heading-art" className={`${classes.firstChapter} ${classes.heading}`} variant="h2" component="h2">
+                <span className={classes.keyword}>KUNST</span> AN DER UNIVERSITÄT BREMEN
+              </Typography>
+              
+              <Typography component="div" className={`${classes.content} ${classes.firstContent}`} dangerouslySetInnerHTML={{ __html: getTextStyle(0) }} />
+            </Container>
+          </section>
+
+          <section className={`${classes.section} ${classes.program}`} aria-labelledby="heading-program">
+            <Container maxWidth="lg">
+              <Grid container spacing={3} alignItems="center">
+                <Grid item sm={12} md={6}>
+                  <img className={classes.image} alt={getImageAlt(1)} src={`${process.env.PUBLIC_URL}/img/${getImage(1)}`} />
+                </Grid>
+                <Grid item sm={12} md={6}>
+                  <Typography id="heading-program" variant="h3" component="h2" className={classes.heading}>
+                    <span className={classes.keyword}>PROGRAMM</span> "KUNST IM UNIVERSITÄTSBEREICH"
+                  </Typography>
+                  <Typography component="div" className={classes.content} dangerouslySetInnerHTML={{ __html: getTextStyle(1) }} />
+                </Grid>
+              </Grid>
+            </Container>
+          </section>
+
+          <section className={`${classes.section} ${classes.goals}`} aria-labelledby="heading-goals">
+            <Container maxWidth="lg">
+              <Grid container spacing={3} alignItems="center">
+                <Grid item sm={12} md={6}>
+                  <Typography id="heading-goals" variant="h3" component="h2" className={classes.heading}>
+                    <span className={classes.keyword}>ZIELE</span> DES BREMER HOCHSCHULMODELLS
+                  </Typography>
+                  <Typography component="div" className={classes.content} dangerouslySetInnerHTML={{ __html: getTextStyle(2) }} />
+                </Grid>
+                <Grid item sm={12} md={6}>
+                  <img className={classes.image} alt={getImageAlt(2)} src={`${process.env.PUBLIC_URL}/img/${getImage(2)}`} />
+                </Grid>
+              </Grid>
+            </Container>
+          </section>
+
+          <section className={`${classes.section} ${classes.premises}`} aria-labelledby="heading-premises">
+            <Container maxWidth="lg">
+              <Grid container spacing={3} alignItems="center">
+                <Grid className={classes.premiseListImg} item sm={12} md={6}>
+                  <img className={classes.image} alt={getImageAlt(3)} src={`${process.env.PUBLIC_URL}/img/${getImage(3)}`} />
+                </Grid>
+                <Grid className={classes.premiseList} item sm={12} md={6}>
+                  <div>
+                    <Typography id="heading-premises" variant="h3" component="h2" className={classes.heading}>
+                      ES GALTEN ZWEI <span className={classes.keyword}>PRÄMISSEN</span>
+                    </Typography>
+                    <Typography component="div" className={classes.content} dangerouslySetInnerHTML={{ __html: getTextStyle(3) }} />
+                  </div>
+                </Grid>
+              </Grid>
+            </Container>
+          </section>
+
+          <section className={`${classes.section} ${classes.exhibition}`} aria-labelledby="heading-exhibition">
+            <Container maxWidth="lg">
+              <Grid container spacing={3} alignItems="center">
+                <Grid item sm={12} md={6}>
+                  <Typography id="heading-exhibition" variant="h3" component="h2" className={classes.heading}>
+                     AUSSTELLUNG <span className={classes.keyword}>KUNST IM STADTBILD</span>
+                  </Typography>
+                  <Typography component="div" className={classes.content} dangerouslySetInnerHTML={{ __html: getTextStyle(4) }} />
+                </Grid>
+                <Grid item sm={12} md={6}>
+                  <img className={classes.image} alt={getImageAlt(4)} src={`${process.env.PUBLIC_URL}/img/${getImage(4)}`} />
+                </Grid>
+              </Grid>
+            </Container>
+          </section>
+        </main>
+      </Fade>
+    </Fragment>
+  );
 };
 
-export default start;
+export default Start;
